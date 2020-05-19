e0df58ec7131b36954095e16c4cd8a5867a9b7ea
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-05-19 00:15:38 -0300

    Create JSON file of frontend/vagas

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const fetch = require('node-fetch');
+
+const GITHUB_FRONTEND_VAGAS = (
+  'https://api.github.com/repos/frontendbr/vagas/issues'
+);
+
+fetch(GITHUB_FRONTEND_VAGAS).then((response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+}).then((response) => {
+  return response.map(
+    ({title, body, html_url, created_at}) => {
+      return {
+        title,
+        description: body,
+        url: html_url,
+        published_at: created_at
+      };
+    }
+  );
+}).then((response) => {
+  fs.writeFileSync('./jobs.json', JSON.stringify(response), 'utf-8');
+});
