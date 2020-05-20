f5a6d26c5d8dbfaae0e4df23564772a5a9325132
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-05-20 17:50:00 -0300

    Filter jobs of previous date

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
+const datetime = require('node-datetime');
 
 const fetch = require('node-fetch');
 
+const DATE_FORMAT_STRING = 'Y-m-dT00:00:00Z';
+const DATE_TODAY_STRING = datetime.create().format(DATE_FORMAT_STRING);
+const DATE_TODAY = datetime.create(DATE_TODAY_STRING);
+const DATE_YESTERDAY = datetime.create(DATE_TODAY_STRING);
+DATE_YESTERDAY.offsetInDays(-1);
+const DATE_YESTERDAY_STRING = DATE_YESTERDAY.format(DATE_FORMAT_STRING);
+
 const GITHUB_FRONTEND_VAGAS = (
-  'https://api.github.com/repos/frontendbr/vagas/issues'
+  'https://api.github.com/repos/frontendbr/vagas/issues?since='
+  + DATE_YESTERDAY_STRING
 );
 
 fetch(GITHUB_FRONTEND_VAGAS).then((response) => {
@@ -19,10 +28,17 @@ fetch(GITHUB_FRONTEND_VAGAS).then((response) => {
         title,
         description: body,
         url: html_url,
-        publishedAt: created_at
+        publishedAt: new Date(datetime.create(created_at).getTime())
       };
     }
   );
-}).then((response) => {
+}).then((response) => response.filter(
+  (job) => {
+    return (
+      (job.publishedAt.getTime() > DATE_YESTERDAY.getTime())
+      && (job.publishedAt.getTime() < DATE_TODAY.getTime())
+    );
+  }
+)).then((response) => {
   fs.writeFileSync('./jobs.json', JSON.stringify(response), 'utf-8');
 });
