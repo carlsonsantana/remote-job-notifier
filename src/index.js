89821895e485e68367fbc94ab2bf06a6134aca68
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-05-24 02:38:54 -0300

    Fix error when there aren't jobs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const dialog = require('dialog');
 const getGitHubJobsPromise = require('./github.js');
 
 getGitHubJobsPromise().then((jobs) => {
+  if (jobs.length === 0) {
+    return;
+  }
+
   const message = jobs.reduce((accumulator, job) => {
     const endString = job.title + '\n' + job.url + '\n\n';
     if (typeof accumulator === 'string') {
