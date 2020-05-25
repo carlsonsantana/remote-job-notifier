25f0169bb582e3d8292f86efa36d854d62f151d0
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-05-25 20:26:57 -0300

    Define the since value

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const dialog = require('dialog');
 const jobs = require('jobs-promise');
 
-jobs.githubIssuesPromise().then((jobs) => {
+const YESTERDAY = new Date(Date.now() - 86400000);
+YESTERDAY.setMilliseconds(0);
+YESTERDAY.setSeconds(0);
+YESTERDAY.setMinutes(0);
+YESTERDAY.setHours(0);
+
+jobs.githubIssuesPromise(YESTERDAY).then((jobs) => {
   const message = jobs.map((job) => job.title + '\n' + job.url).join('\n\n');
 
   if (message.length > 0) {
