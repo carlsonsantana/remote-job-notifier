f75d2b7d9f0fe5dedf40924fe149b7755de07ab8
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-05-25 20:36:27 -0300

    Define the repositories value

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const dialog = require('dialog');
 const jobs = require('jobs-promise');
 
+const REPOSITORIES = [
+  {owner: 'frontendbr', repo: 'vagas'},
+  {owner: 'backend-br', repo: 'vagas'}
+];
+
 const YESTERDAY = new Date(Date.now() - 86400000);
 YESTERDAY.setMilliseconds(0);
 YESTERDAY.setSeconds(0);
 YESTERDAY.setMinutes(0);
 YESTERDAY.setHours(0);
 
-jobs.githubIssuesPromise(YESTERDAY).then((jobs) => {
+jobs.githubIssuesPromise(REPOSITORIES, YESTERDAY).then((jobs) => {
   const message = jobs.map((job) => job.title + '\n' + job.url).join('\n\n');
 
   if (message.length > 0) {
